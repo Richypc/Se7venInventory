32541662e61fe1e45d2c49e985ebd57ac916dd38
Richard Porras C <richypc_4@hotmail.com>
2022-04-28 14:17:41 -0500

    Creacion de clases por tableadapter

--- Se7venInventory/CADSe7ven/CADThird.cs
+++ Se7venInventory/CADSe7ven/CADThird.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using CADSe7ven.DSSev7venTableAdapters;
+using System.Data.SqlClient;
 namespace CADSe7ven
 {
-    internal class CADThird
+    public class CADThird
     {
+        private static ThirdTableAdapter adaptador = new ThirdTableAdapter();
+
+        public static bool ExisteThird(string ThirdNumberId)
+        {
+            return adaptador.ExisteThird(ThirdNumberId);
+        }
 
     }
 }
---
+++ Se7venInventory/CADSe7ven/ThirdTableAdapter.cs
@@ -0,0 +1,10 @@
+﻿namespace CADSe7ven
+{
+    internal class ThirdTableAdapter
+    {
+        internal bool ExisteThird(string thirdNumberId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
